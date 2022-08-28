2515df5c02733dee9b8754c25fb3f604daff2694
Krishna Ravishankar <25255817+krsh732@users.noreply.github.com>
2022-08-28 16:45:58 -0400

    Dump stdout/stderr for cgame/game building

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-from subprocess import check_output, STDOUT
+from subprocess import check_output, STDOUT, CalledProcessError
 from zipfile import ZipFile
 from shutil import rmtree
 from os import makedirs
@@ -6,8 +6,16 @@ from sys import executable
 
 rmtree("build", ignore_errors=True)
 makedirs("build", exist_ok=True)
-check_output([executable, "src/cgame/cgame.py"], stderr=STDOUT)
-check_output([executable, "src/qagame/qagame.py"], stderr=STDOUT)
+def build_vm(vm):
+    print(f"{vm}:")
+    try:
+        print(check_output([executable, f"src/{vm}/{vm}.py"], stderr=STDOUT).decode("utf8"))
+    except CalledProcessError as e:
+        print(e.output.decode("utf8"))
+    print("---------------------------------------------------------------------------")
+
+build_vm("cgame")
+build_vm("qagame")
 with ZipFile("build/zzzzz-patched-vms.pk3", "w") as z:
     z.write("build/cgame.qvm", "vm/cgame.qvm")
     z.write("build/qagame.qvm", "vm/qagame.qvm")
